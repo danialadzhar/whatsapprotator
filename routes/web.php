9d720b8e3ec65d2d8d6d6df9b4c7204ad0432332
danialadzhar <danial0597@gmail.com>
2020-06-01 16:45:32 +0800

    Open Password

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@
 
 use Illuminate\Support\Facades\Hash;
 
-/*
+
 Route::get('pass', function () {
     echo Hash::make('brandname');
 });
 
-
+/*
 Route::get('pm', function () {
     return view('welcome');
 });
