d3b6ff5f5de43f9df15d8bbe329c40327d965096
danialadzhar <danial0597@gmail.com>
2020-06-03 10:05:00 +0800

    Added migration

---
+++ database/migrations/2020_06_03_020156_create_whatsappcampaign_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWhatsappcampaignTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('whatsappcampaign', function (Blueprint $table) {
+            $table->id();
+            $table->string('whatsapp_campaign_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('program');
+            $table->text('whatsapp_answer');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('whatsappcampaign');
+    }
+}
